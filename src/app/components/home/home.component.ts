da5e75392c9b25dd3f0bf33c64069c72cb31833e
Ahmet Faruk Cali <1335017@dogus.edu.tr>
2019-11-11 23:02:58 +0300

    register button visibility managed

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  AfterViewChecked,
+  DoCheck
+} from "@angular/core";
 import { User } from "../../models/userModel";
 import * as CryptoJS from "crypto-js";
 import { ToastrService } from "ngx-toastr";
@@ -8,16 +14,30 @@ import { ToastrService } from "ngx-toastr";
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.scss"]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, DoCheck {
   doctorName: string;
   clinicName: string;
   password: string;
   rePassword: string;
+  buttonVisible: boolean = true;
   secret: string =
     "4107E215B2E4907348E67E4B77FA7CC0DF1897DB342316520DBA5ED9CB0E1C1B";
   constructor(private toastr: ToastrService) {}
 
   ngOnInit() {}
+
+  ngDoCheck() {
+    if (
+      this.doctorName &&
+      this.clinicName &&
+      this.password &&
+      this.rePassword
+    ) {
+      this.buttonVisible = false;
+    } else {
+      this.buttonVisible = true;
+    }
+  }
 
   register(): any {
     if (this.password !== this.rePassword) {
