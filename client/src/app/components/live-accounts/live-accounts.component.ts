a10f6d882b56f2ce3e7e391f87176f471f477405
Ahmet Faruk Cali <1335017@dogus.edu.tr>
2019-11-15 22:51:55 +0300

    Added simple live accounts page

--- client/src/app/components/live-accounts/live-accounts.component.ts
+++ client/src/app/components/live-accounts/live-accounts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Patient } from "../../models/patientModel";
 
 @Component({
   selector: "app-live-accounts",
@@ -6,7 +7,20 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./live-accounts.component.scss"]
 })
 export class LiveAccountsComponent implements OnInit {
+  patients: any[];
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.patients = [
+      { name: "#patient1" },
+      { name: "#patient2" },
+      { name: "#patient3" },
+      { name: "#patient4" },
+      { name: "#patient5" },
+      { name: "#patient6" },
+      { name: "#patient7" },
+      { name: "#patient8" },
+      { name: "#patient9" }
+    ];
+  }
 }
