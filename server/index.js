7192860f6175242facf14056926369b80c6b233f
Ahmet Faruk Cali <1335017@dogus.edu.tr>
2019-11-19 23:51:04 +0300

    Mongo db connection completed

--- server/index.js
+++ server/index.js
@@ -1,14 +1,29 @@
 const express = require("express"),
-  bodyParser = require("body-parser"),
-  authController = require("./controllers/auth.controller");
+	bodyParser = require("body-parser"),
+	authController = require("./controllers/auth.controller"),
+	dbConfig = require("./config/database_config.js");
+const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
+
+mongoose.Promise = global.Promise;
 
 app.use(cors());
 app.options("*", cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 authController(app);
+
+mongoose
+	.connect(dbConfig.url, {
+		useNewUrlParser: true
+	})
+	.then(() => {
+		console.log("Successfully connected to the database");
+	})
+	.catch(err => {
+		console.log("Could not connect to the database. Exiting now...", err);
+		process.exit();
+	});
 
 app.listen(4201);
