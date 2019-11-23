2a3605bb832184b1c0ead222eed4798d4065d153
Ahmet Faruk Cali <1335017@dogus.edu.tr>
2019-11-23 15:14:20 +0300

    Removed qr actions

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,22 +1,11 @@
 import { Component, OnInit } from "@angular/core";
-import { BarcodeFormat } from "@zxing/library";
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
-  hash: string;
-  allowedFormats = [];
   constructor() {}
 
-  ngOnInit() {
-    this.hash = history.state.data.registeredUser.hashWithoutPassword;
-    this.allowedFormats = [
-      BarcodeFormat.QR_CODE,
-      BarcodeFormat.EAN_13,
-      BarcodeFormat.CODE_128,
-      BarcodeFormat.DATA_MATRIX /*, ...*/
-    ];
-  }
+  ngOnInit() {}
 }
